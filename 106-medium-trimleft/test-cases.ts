cfc15e3f217154142e37d6cd00df7bcccf31ea12
Pakho Leung <pakholeung37@gmail.com>
2021-06-06 22:27:48 +0800

    feat: kill some challenges

--- 106-medium-trimleft/test-cases.ts
+++ 106-medium-trimleft/test-cases.ts
@@ -1,5 +1,8 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type TrimLeft<T extends string> = T extends `${' ' | '\n' | '\t'}${infer P}`
+  ? TrimLeft<P>
+  : T
 type cases = [
   Expect<Equal<TrimLeft<'str'>, 'str'>>,
   Expect<Equal<TrimLeft<' str'>, 'str'>>,
--- 62-medium-type-lookup/test-cases.ts
+++ 62-medium-type-lookup/test-cases.ts
@@ -1,5 +1,6 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type LookUp<T, K extends string> = T extends { type: K } ? T : never
 interface Cat {
   type: 'cat'
   breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
@@ -13,6 +14,7 @@ interface Dog {
 
 type Animal = Cat | Dog
 
+type a = LookUp<Animal, 'dog'>
 type cases = [
   Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
   Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
